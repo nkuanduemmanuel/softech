8fb173c369c2bc12aa78a3ea63bf67f095948c69
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-08-17 16:55:39 -0700

    Update Grains.java

--- Grains.java
+++ Grains.java
@@ -10,9 +10,8 @@ import java.io.IOException;
 public class Grains {
 	public static void main ( String arg[])	throws IOException{
 	
-		BigInteger TotalNumberOfGrains = BigInteger.valueOf(0);
-		BigInteger NumberOfGrains = BigInteger.valueOf ( 1 );
-		
+		BigInteger totalNumberOfGrains = BigInteger.valueOf(0);
+		BigInteger numberOfGrains = BigInteger.valueOf ( 1 );
 		
 		for (  int i = 1; i <= 64; i++ ) {					
 			String output = " square " +  i + ":" + "\t" + NumberOfGrains + "\t" + "grains" + "\n" ;			
