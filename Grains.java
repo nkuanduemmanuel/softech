4925edaa4935ea94e3d0b99bf985f847aae657ba
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-08-19 06:39:05 -0700

    Update Grains.java

--- Grains.java
+++ Grains.java
@@ -8,17 +8,18 @@ import java.io.IOException;
  */
 
 public class Grains {
-	public static void main ( String arg[])	throws IOException{
+	public static void main (String args[])	throws IOException{
 	
 		BigInteger totalNumberOfGrains = BigInteger.valueOf(0);
-		BigInteger numberOfGrains = BigInteger.valueOf ( 1 );
+		BigInteger numberOfGrains = BigInteger.valueOf(1);
 		
-		for (  int i = 1; i <= 64; i++ ) {					
+		for (int i = 1; i <= 64; i++) {					
 			String output = " square " +  i + ":" + "\t" + NumberOfGrains + "\t" + "grains" + "\n" ;			
-			System.out.print( output ); 
+			System.out.print(output); 
 			totalNumberOfGrains = totalNumberOfGrains.add(numberOfGrains);	
-			numberOfGrains = numberOfGrains.multiply( BigInteger.valueOf( 2 ) );
-		}		
-		System.out.print( "The total number of grains on the entire chess board is" + "\t" + totalNumberOfGrains );				
+			numberOfGrains = numberOfGrains.multiply(BigInteger.valueOf(2));
+		}
+		
+		System.out.print("The total number of grains on the entire chess board is" + "\t" + totalNumberOfGrains);				
 	}
 }
