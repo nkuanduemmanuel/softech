948df3152fea3a44928d53d5c795dd509c7b5bb8
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-08-19 07:11:59 -0700

    Update Times.java

--- Times.java
+++ Times.java
@@ -4,24 +4,18 @@
  * @author Emmanuel Nkuandu <nkuandu.emmanuel@hotmail.com>
  */
   public class Times {
-    
-    public static void main ( String arg[]){
-	        System.out.print( "\t" );
-	   
-	        for( int i = 0; i <= 9; i++) {		   
-		    System.out.print( i + "\t" );
+    public static void main ( String args[]){
+	        System.out.print("\t");
+	        for(int i = 0; i <= 9; i++){		   
+		    System.out.print(i + "\t");
 	           }
-	   
-	    System.out.print( "\n" );
-	    
-	   for ( int j = 0; j <= 9; j++) {   
-		   System.out.print( j + "\t");
-		   
-		   for (int k = 0; k <= 9; k++) {
+	    System.out.print("\n");
+	    for (int j = 0; j <= 9; j++) {   
+		 System.out.print( j + "\t");
+		   for (int k = 0; k <= 9; k++){
 			  System.out.print(j*k + "\t");			  
 		   }
-		   
-		   System.out.print("\n");
+		 System.out.print("\n");
 	   }
-   }
+      }
 }
