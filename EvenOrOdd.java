585bbc0c71f7bb7db7b3391f868f2bfca852b0eb
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-09-05 04:26:11 -0700

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -10,16 +10,15 @@ import java.io.InputStreamReader;
 public class EvenOrOdd {
 	
     public static void main(String args[]) throws IOException{
-		System.out.print("Type in a number!");			
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-                String userInput = reader.readLine();			
-		 
-		int number = Integer.parseInt(userInput);	
-		if (number % 2 == 0) {
-			System.out.print("Even");
-		} else {
-			System.out.print("Odd"); 
-		}		
-	      } 
-            }
+    	
+	System.out.print("Type in a number!");
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+            String userInput = reader.readLine();
+	    int number = Integer.parseInt(userInput);	
+	    if (number % 2 == 0) {
+		System.out.print("Even");
+	    } else {
+		System.out.print("Odd"); 
+	}		
+    } 
+}
