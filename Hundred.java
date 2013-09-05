0fe2308ccaf89098a9820efc1e63a7ab85cf8873
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-09-05 05:11:21 -0700

    Update Hundred.java

--- Hundred.java
+++ Hundred.java
@@ -7,13 +7,13 @@ import java.io.*;
  */
 
 public class Hundred {
-	public static void main (String[] args) throws IOException {
-	  System.out.println("Type some text and press 'Enter.'");		 
-	  BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-
-	  String UserInput = reader.readLine();		 
-          for(int i= 1; i <= 100; i++) {
-       	  System.out.print( UserInput + "\t" );
+	
+    public static void main (String[] args) throws IOException{
+        System.out.println("Type some text and press 'Enter.'");		 
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+	String userInput = reader.readLine();		 
+        for (int i= 1; i <= 100; i++) {
+       	     System.out.print(userInput + "\t");
        }
     }
  }
