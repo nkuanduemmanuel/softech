1752824295f022d00ccfee786f2f90f16c8d834d
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-09-13 08:43:40 -0700

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,14 +3,15 @@
 *
 * @author Emmanuel Nkuandu <nkuandu.emmanuel@hotmail.com>
 */
+
 public class Sum {
-	public static void main (String args[]){
+	public static void main(String args[]) {
 		double sum = 0.0;
-		for (int i = 0; i <= 1000; i++){
-			sum += 0.1; 
+		for (int i = 0; i <= 1000; i++) {
+			sum += 0.1;
 		}
 		System.out.print(sum + "\n");
-		if (sum == 100){
+		if (sum == 100) {
 			System.out.print("The sum is equal to 100");
 		} else {
 			System.out.print("The sum is not equal to 100");
