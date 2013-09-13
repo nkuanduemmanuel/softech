f1ae01f3c0201300060542a5b7c117252748c64f
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-09-13 08:29:11 -0700

    Update Hundred.java

--- Hundred.java
+++ Hundred.java
@@ -2,18 +2,17 @@ import java.io.*;
 
 /**
  * Utility class to print user input 100 times.
- *
+ * 
  * @author Emmanuel Nkuandu <nkuandu.emmanuel@hotmail.com>
  */
 
 public class Hundred {
-	
-    public static void main (String[] args) throws IOException{
-        System.out.println("Type some text and press 'Enter.'");		 
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-	String userInput = reader.readLine();		 
-        for (int i= 1; i <= 100; i++) {
-       	     System.out.print(userInput + "\t");
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		System.out.println("Type some text and press 'Enter.'");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userInput = reader.readLine();
+		for (int i = 1; i <= 100; i++) {
+			System.out.print(userInput + "\t");
+		}
+	}
 }
