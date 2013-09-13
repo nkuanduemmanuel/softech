e7cce200d5852b587dcffb91c0cc9b582d2cd2a7
nkuanduemmanuel <nkuandu.emmanuel@gmail.com>
2013-09-13 08:04:44 -0700

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -4,21 +4,21 @@ import java.io.InputStreamReader;
 
 /**
  * Utility class for determining whether a number is even or odd.
- *
+ * 
  * @author Emmanuel Nkuandu <nkuandu.emmanuel@hotmail.com>
  */
+
 public class EvenOrOdd {
-	
-    public static void main(String args[]) throws IOException{
-    	
-	System.out.print("Type in a number!");
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-            String userInput = reader.readLine();
-	    int number = Integer.parseInt(userInput);	
-	    if (number % 2 == 0) {
-		System.out.print("Even");
-	    } else {
-		System.out.print("Odd"); 
-	}		
-    } 
+	public static void main(String args[]) throws IOException {
+		System.out.print(" Type in a number! ");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userInput = reader.readLine();
+		int number = Integer.parseInt(userInput);
+		if (number % 2 == 0) {
+			System.out.print("Even");
+		} else {
+			System.out.print("Odd");
+		}
+	}
 }
